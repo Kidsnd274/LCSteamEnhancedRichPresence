dcc5e3ada9a1365cb90c9434c2429501632d6582
Kidsnd274 <samuel_tee_gy@hotmail.com>
2023-12-01 15:04:16 +0800

    Initial code for plugin. Currently not working, trying to interface with (potentially created) SteamManager or SteamWorks.

--- LCSteamEnhancedRichPresence/Plugin.cs
+++ LCSteamEnhancedRichPresence/Plugin.cs
@@ -1,14 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
 
 namespace LCSteamEnhancedRichPresence
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        internal static ManualLogSource log;
         private void Awake()
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            log = Logger;
+            log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            GameObject steamErpGameObject = new GameObject();
+            steamErpGameObject.AddComponent<SteamERP>();
+            DontDestroyOnLoad(steamErpGameObject);
+            steamErpGameObject.hideFlags = HideFlags.HideAndDontSave;
+            log.LogInfo("Created SteamERP Object");
         }
     }
 }
---
+++ LCSteamEnhancedRichPresence/SteamERP.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using System;
+using Steamworks;
+using UnityEngine;
+
+namespace LCSteamEnhancedRichPresence;
+public class SteamERP : MonoBehaviour
+{
+    private void Awake() {
+        if (SteamManager.Instance == null) {
+            var steamManagerGameObject = new GameObject("SteamManager");
+            steamManagerGameObject.AddComponent<SteamManager>();
+            DontDestroyOnLoad(steamManagerGameObject);
+            Plugin.log.LogInfo("Created SteamManager GameObject");
+        }
+    }
+
+    private void Start() {
+        Plugin.log.LogInfo("SteamERP Object Start()");
+        if (SteamManager.Instance != null) {
+            UpdateRichPresence();
+            Plugin.log.LogInfo("RichPresence set");
+        } else {
+            Plugin.log.LogInfo("SteamManager Instance was null");
+        }
+        
+    }
+    
+    private void UpdateRichPresence() {
+        SteamFriends.SetRichPresence("status", "current writing a mod for LC");
+    }
+}
