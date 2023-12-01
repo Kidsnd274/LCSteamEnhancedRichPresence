221ad185c917409b07323885a4a69ddce928a224
Kidsnd274 <samuel_tee_gy@hotmail.com>
2023-12-01 23:59:30 +0800

    First implementation of ERP - Waits for the first mainmenu load for SteamAPI to initialize - Mod can now detect if Online or LAN mode - ERP is sending to Steam currently
    Mod doesn't work since the game developer has to setup localization for the Rich Presenece keys. The only thing that works is the `steam_player_group`.
    
    "steam_display" - Names a rich presence localization token that will be displayed in the viewing user's selected language in the Steam client UI. See Rich Presence Localization for more info, including a link to a page for testing this rich presence data. If steam_display is not set to a valid localization tag, then rich presence will not be displayed in the Steam client.
    Source: https://partner.steamgames.com/doc/api/ISteamFriends#SetRichPresence

--- LCSteamEnhancedRichPresence/SteamERP.cs
+++ LCSteamEnhancedRichPresence/SteamERP.cs
@@ -1,32 +1,89 @@
 ﻿using BepInEx;
-using System;
+using System.Collections;
 using Steamworks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace LCSteamEnhancedRichPresence;
+
 public class SteamERP : MonoBehaviour
 {
+    bool modInitialized = false; // Only start script if the game goes into main menu, check if SteamClient is initialised, if not disable script (might go LAN mode)
+
     private void Awake() {
-        if (SteamManager.Instance == null) {
-            var steamManagerGameObject = new GameObject("SteamManager");
-            steamManagerGameObject.AddComponent<SteamManager>();
-            DontDestroyOnLoad(steamManagerGameObject);
-            Plugin.log.LogInfo("Created SteamManager GameObject");
-        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
 
     private void Start() {
-        Plugin.log.LogInfo("SteamERP Object Start()");
-        if (SteamManager.Instance != null) {
-            UpdateRichPresence();
-            Plugin.log.LogInfo("RichPresence set");
-        } else {
-            Plugin.log.LogInfo("SteamManager Instance was null");
-        }
-        
+        Plugin.log.LogDebug("SteamERP Started... Waiting for Network Initialization");
+        // Get GameNetworkManager and check disableSteam
+        // create initialize function only if disableSteam is false and after OnSceneLoaded mainmenu for the first time.
+        // if disablesteam is true, consider destroying this gameobject to disable mod until next boot
     }
     
+    private void Update() {
+
+    }
+
     private void UpdateRichPresence() {
-        SteamFriends.SetRichPresence("status", "current writing a mod for LC");
+        if (CheckIfSteamDisabled()) {
+            Plugin.log.LogWarning("Attempted to SetRichPresence while Steam is disabled!");
+            return;
+        }
+        try {
+            SteamFriends.ClearRichPresence();
+            bool result = SteamFriends.SetRichPresence("steam_display", "current writing a mod for LC");
+            Plugin.log.LogInfo("RP returns " + result.ToString());
+            result = SteamFriends.SetRichPresence("status", "test");
+            Plugin.log.LogInfo("RP returns " + result.ToString());
+            result = SteamFriends.SetRichPresence("steam_player_group", "test");
+            SteamFriends.SetRichPresence("steam_player_group_size", "2000");
+            Plugin.log.LogInfo("RP returns " + result.ToString());
+            //SteamFriends.OpenOverlay("Friends");
+            SteamClient.RunCallbacks();
+            Plugin.log.LogInfo("RP set");
+        } catch (System.Exception e) {
+            Plugin.log.LogError(e.Message);
+        }
+
+    }
+
+    private bool CheckIfSteamDisabled() { // Online or LAN mode
+        return GameNetworkManager.Instance.disableSteam;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        if (!modInitialized) { // Initialize mod on the first load of main menu
+            var currentScene = scene.name;
+            if (currentScene is "MainMenu") {
+                if (!CheckIfSteamDisabled()) {
+                    Plugin.log.LogInfo("Steam is enabled!");
+                    UpdateRichPresence();
+                } else {
+                    Plugin.log.LogInfo("Steam is disabled!");
+                    Destroy(this.gameObject);
+                }
+            }
+            // DO NOTHING HERE BECAUSE MOD IS NOT INITALIZED!
+            return;
+        }
+        Plugin.log.LogInfo("HELLO");
+        var name = SteamClient.SteamId;
+        Plugin.log.LogInfo(name);
+        Plugin.log.LogInfo("Game should be running");
+        UpdateRichPresence();
+        modInitialized = true;
+    }
+
+    private void OnSceneUnloaded(Scene scene) {
+
+    }
+
+    private void OnDestroy() {
+        SteamClient.Shutdown();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+        Plugin.log.LogInfo("Mod disabled");
     }
 }
